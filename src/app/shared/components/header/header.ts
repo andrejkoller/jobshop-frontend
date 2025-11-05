4f342c7955301d7d227f1e8c1ae4cef2870c8fbf
Andrej Koller <andrejkoller@outlook.com>
2025-11-05 18:52:19 +0100

    feat(header): add mobile navigation button with menu icon style(login, signup): change width to fit-content and adjust media queries

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,16 +1,18 @@
 import { Component, HostListener, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgClass } from '@angular/common';
+import { LucideAngularModule, MenuIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, NgClass],
+  imports: [RouterLink, NgClass, LucideAngularModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header implements OnInit {
   protected readonly title = 'jobshop';
   protected isScrolled = signal(false);
+  protected readonly MenuIcon = MenuIcon;
 
   ngOnInit() {
     this.checkScrollPosition();
